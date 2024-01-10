a020c1cd7404f2d3af322f14270cf3ec58766d82
same-hack <samehack.info@gmail.com>
2024-01-10 22:40:23 +0900

    fc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,23 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular17_for';
+  // ユーザリストの宣言
+  userList: any[] = [];
+
+  /**ユーザ追加処理  */
+  addUser() {
+    // ユーザIDを定義
+    const userId = this.userList.length + 1;
+    // ユーザ情報を定義
+    const user = {
+      id: userId,
+      name: `ユーザ${userId}`,
+    };
+
+    // ユーザを追加
+    this.userList.push(user);
+  }
 }
